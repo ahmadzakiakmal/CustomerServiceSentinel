4281b8b1e54e8f0655c8824fc61f5f35bb8e1197
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-02-20 08:29:23 +0700

    feat: basic setup

--- backend/app.py
+++ backend/app.py
@@ -1,18 +1,56 @@
-from flask import Flask, request
+from flask import Flask, jsonify
 from openai import OpenAI
 import os
+from flask_pymongo import PyMongo
+from mongoengine.base.common import get_document
+import models
+
+User = get_document("User")
+Organization = get_document("Organization")
 
 from dotenv import load_dotenv
 load_dotenv()
-print(os.environ.get("OPENAI_API_KEY"))
-if not os.environ.get("OPENAI_API_KEY"):
-  raise ValueError("API Key not found.")  
+
+if not os.environ.get("OPENAI_API_KEY") or not os.environ.get("MONGODB_URI"):
+  raise ValueError("Required environment variables are not found.")  
 
 app = Flask(__name__)
+app.config["MONGO_URI"] = os.environ.get("MONGODB_URI")
+mongo = PyMongo(app)
+
+# Register Blueprints
+from blueprints.user.user_bp import user_bp
+app.register_blueprint(user_bp, url_prefix="/user")
 
 if __name__ == '__main__':
   app.run(debug=True)
 
+@app.errorhandler(Exception)
+def handle_exception(e):
+  return jsonify({
+        "code": 500,
+        "message": "Unhandled Exception",
+        "description": str(e),
+    }), 500
+
 @app.route("/")
 def hello_world():
-  return "<div style='min-height: 100vh; display: flex; justify-content: center; align-items: center; font-size: 20'>CustomerServiceSentinel</div>"
+  return """
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <body>
+    <div style='min-height: 100vh; display: flex; flex-direction: column; justify-content: center; align-items: center; font-size: 24px; font-family: "Poppins"; text-align: center'>
+      <p style='font-weight: 600; margin: 0'>
+        CustomerServiceSentinel 
+        <br /> 
+        Backend
+      </p>
+      <p style='margin: 0; font-size: 20px'>
+        Version 0.0.1
+      </p>
+    </div>
+  </body>
+  """
+
+
