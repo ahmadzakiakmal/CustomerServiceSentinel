d3f7ecf8b396796c36a4a2256ced73e4cc27ba7b
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-02-27 20:48:00 +0700

    fix: improve error handling on auth middleware

--- backend/middlewares/authentications.py
+++ backend/middlewares/authentications.py
@@ -7,12 +7,20 @@ def authenticateUser(f):
   def wrapper(*args, **kwargs):
     try:
       cookies = request.headers.get("Cookie")
-      auth_cookie = cookies.split(";")[0]
+      print(cookies)
+      print("Authentication=" in cookies)
+      if not cookies:
+        raise Unauthorized("No cookie found.")
+      if not "Authentication=" in cookies:
+        raise Unauthorized("No authentication cookie found.")
+      
+      auth_cookie = next((cookie for cookie in cookies.split("; ") if cookie.startswith("Authentication=")), None)
+      if not auth_cookie:
+        raise Unauthorized("Authentication cookie is missing.")
+
       token = auth_cookie.split("=")[1]
-      if not "Authentication" in cookies:
-        raise Unauthorized("No Authentication cookie found.")
       payload = decode(token, os.environ.get("JWT_SECRET"), algorithms=["HS256"])
-      # print(payload)
+      print(payload)
       return f(*args, **kwargs)
     except Unauthorized as e:
       return jsonify({"error" : str(e)}), 401
