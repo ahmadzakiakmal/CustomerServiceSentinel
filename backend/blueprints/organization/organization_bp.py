b997964512499220957e2b92a1d934856d9d5de2
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-03-04 17:09:04 +0700

    feat: add owner field to Organization entity

--- backend/blueprints/organization/organization_bp.py
+++ backend/blueprints/organization/organization_bp.py
@@ -1,17 +1,27 @@
 from flask import Blueprint, request, jsonify
 from app import Organization
 from werkzeug.exceptions import BadRequest
+from middlewares.authentications import authenticateUser
+from jwt import decode
+import os
 
 organization_bp = Blueprint("organization_blueprint", __name__)
 
 @organization_bp.route("/", methods=["POST"])
+@authenticateUser
 def create_organization():
   try:
     data = request.get_json()
+    cookies = request.headers.get("Cookie", "")
+    auth_cookie = next((cookie for cookie in cookies.split("; ") if cookie.startswith("Authentication=")), None)
+    token = auth_cookie.split("=")[1]
+    payload = decode(token, os.environ.get("JWT_SECRET"), algorithms=["HS256"])
+    print(payload.get("email"))
     if not data.get("organizationName"):
       raise BadRequest("Missing required parameters")
     org = Organization(
-      organization_name = data["organizationName"]
+      organization_name = data["organizationName"],
+      owner=payload.get("email")
     )
     org.save()
     return jsonify({
@@ -19,6 +29,7 @@ def create_organization():
       "data": {
         "id": org.id,
         "name": data["organizationName"],
+        "owner": payload.get("email")
       }
     }), 201
   except BadRequest as e:
--- backend/models/organization.py
+++ backend/models/organization.py
@@ -3,5 +3,6 @@ from mongoengine import Document, StringField, ListField, LazyReferenceField, PU
 class Organization(Document):
   organization_name = StringField(required=True, max_length=50)
   chat_records = ListField(LazyReferenceField("Thread", reverse_delete_rule=CASCADE)) 
+  owner = StringField(required=True)
 
   meta = {"collection" : "Organizations"}
