868045053f05e16112467341bec56f3eaefdbb52
Ahmad Zaki Akmal <87590846+ahmadzaki2975@users.noreply.github.com>
2024-03-21 21:03:18 +0700

    feat: prepare endpoint to add instruction

--- backend/blueprints/data/data_bp.py
+++ backend/blueprints/data/data_bp.py
@@ -1,3 +1,8 @@
 from flask import Flask, Blueprint, request, g
 
 data_bp = Blueprint("data_blueprint", __name__)
+
+
+@data_bp.route("/instruction", methods=["POST"])
+def create_instruction():
+  return "create instruction"
