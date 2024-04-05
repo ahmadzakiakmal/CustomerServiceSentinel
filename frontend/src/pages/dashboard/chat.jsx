971a3bf6b42a097be8f77fb51e765a65e97ab14e
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-04-05 09:52:20 +0700

    feat: prepare dashboard sub-pages

---
+++ frontend/src/pages/dashboard/chat.jsx
@@ -0,0 +1,13 @@
+import Layout from "@/components/Layout";
+
+export default function Dashboard() {
+  return(
+    <main>
+      <Layout>
+        <main className="text-dark-brown">
+          <h1 className="text-[24px] font-medium">Chat</h1>
+        </main>
+      </Layout>
+    </main>
+  );
+}
