4316efe43edf817ceb6c2dce587f891a1c541bd7
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-04-05 09:22:23 +0700

    fix: refactor sidebar for less loc
    Co-Authored-By: Athilla Azka Havillah <83878660+athillaazka@users.noreply.github.com>

--- frontend/src/components/Sidebar.js
+++ frontend/src/components/Sidebar.js
@@ -1,77 +1,45 @@
-import classNames from "classnames";
 import { useRouter } from "next/router";
 import Link from "next/link";
 import React, { useState, useMemo } from "react";
 import Image from "next/image";
 import ProfilePicture from "./icons/favicon.ico";
 
-const menuItems = [
-  { id: 1, label: "Dashboard", link: "/" },
-  { id: 2, label: "Chat", link: "./Chat" },
-  { id: 3, label: "Testing", link: "./Testing" },
-  { id: 4, label: "Organization Data", link: "./Organization" },
-];
-
 const Sidebar = () => {
   const router = useRouter();
-  const activeMenu = useMemo(
-    () => menuItems.find((menu) => menu.link === router.pathname),
-    [router.pathname]
-  );
-  const getNavItemClasses = (menu) => {
-    return classNames(
-      "flex items-center cursor-pointer hover:bg-light-brown rounded w-full overflow-hidden whitespace-nowrap",
-      {
-        ["bg-light-lighter"]: activeMenu.id === menu.id,
-      }
-    );
-  };
 
   return (
-    <div className="h-screen flex flex-col justify-between w-full md:w-72 p-4 md:p-8 text-white bg-dark-brown">
-      <div className="flex flex-col items-center">
-        <div className="flex flex-col items-center ml-1">
-          <Image
-            src={ProfilePicture}
-            alt="Profile Picture"
-            width={56}
-            height={56}
-          />
+    <side className="bg-dark-brown w-[275px] h-screen flex flex-col items-center justify-between py-10 text-white">
+      <div className="w-full">
+        <div className="text-white flex flex-col justify-center items-center mb-[75px]">
+          <div className="bg-white size-[60px] rounded-full mb-5" />
+          <h2 className="text-[18px] font-semibold">Username</h2>
         </div>
-        <div className="flex justify-center items-center mt-[24px] mb-[75px]">
-          <div className="flex items-center ">Name</div>
+
+        <div className="flex flex-col gap-10 w-full px-[50px]">
+          <SidebarLink text="Dashboard" link="/dashboard" active={router.pathname === "/dashboard"} />
+          <SidebarLink text="Chat" link="/dashboard/chat" active={router.pathname === "/dashboard/chat"} />
+          <SidebarLink text="Testing" link="/dashboard/testing" active={router.pathname === "/dashboard/testing"} />
         </div>
       </div>
-      <div className="flex flex-col items-center"></div>
-      {menuItems.map(({ icon: Icon, ...menu }) => {
-        const classes = getNavItemClasses(menu);
-        return (
-          <div className={`${classes} mb-10`} key={menu.id}>
-            <Link legacyBehavior href={menu.link}>
-              <a className="flex py-4 px-3 items-center w-full h-full">
-                <span
-                  className={classNames(
-                    "text-md font-medium text-white items-center relative"
-                  )}
-                >
-                  {menu.label}
-                </span>
-              </a>
-            </Link>
-          </div>
-        );
-      })}
-      <div
-        className={`${getNavItemClasses(
-          {}
-        )} px-3 py-4 justify-center sm:mt-10 md:mt-20 lg:mt-72`}
-      >
-        <span className={classNames("text-md font-medium text-text-white")}>
-          Logout
-        </span>
+
+      <div className="w-full px-[50px]">
+        <button className="block text-[18px] font-medium rounded-[5px] px-3 py-2 hover:bg-light-yellow/20 active:bg-light-yellow/50 transition w-full">Log Out</button>
       </div>
-    </div>
+    </side>
   );
 };
+
+function SidebarLink({ text, link, active = false }) {
+  return (
+    <Link
+      className={`block text-[18px] font-medium rounded-[5px] px-3 py-2 hover:bg-light-yellow/20 transition 
+      ${active ? "bg-light-yellow/40" : ""}`
+      }
+      href={link}
+    >
+      {text}
+    </Link>
+  );
+}
 
 export default Sidebar;
