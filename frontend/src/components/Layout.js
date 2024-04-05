418a424e5a95df5d2030837f5b0b191e028e4259
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-04-05 09:48:28 +0700

    fix: children passing not working
    Co-Authored-By: Athilla Azka Havillah <83878660+athillaazka@users.noreply.github.com>

--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,11 +1,11 @@
 import React from "react";
 import Sidebar from "./Sidebar";
 
-function Layout({ Children }) {
+function Layout({ children }) {
   return (
     <div className="h-screen flex flex-row justify-start text-dark-brown">
       <Sidebar />
-      <div className="bg-white-bg flex-1 p-4 text-dark-brown">{Children}</div>
+      <div className="bg-white flex-1 p-10 text-dark-brown">{children}</div>
     </div>
   );
 }
