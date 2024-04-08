53217ab6acf3e9258acabda565856307ddb267e8
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-04-08 21:17:53 +0700

    feat: done Get Assistant Data endpoint (closes #71)

--- backend/blueprints/data/data_bp.py
+++ backend/blueprints/data/data_bp.py
@@ -1,10 +1,11 @@
 from flask import Flask, Blueprint, request, g, jsonify
 from models import Organization, AssistantData
 from werkzeug.exceptions import NotFound, BadRequest
+from mongoengine.errors import DoesNotExist
 
 assistant_data_bp = Blueprint("data_blueprint", __name__)
 
-@assistant_data_bp.route("/instruction/<id>", methods=["PATCH"])
+@assistant_data_bp.route("/<id>", methods=["PATCH"])
 def update_instruction(id):
   body = request.get_json()
   if not body.get("instruction") and body.get("name"):
@@ -18,3 +19,10 @@ def update_instruction(id):
     assistant_data.name = body.get("name")
   assistant_data.save()
   return jsonify({"message": "Updated instruction successfully", "assistant-data": assistant_data}), 200
+
+@assistant_data_bp.route("/<id>")
+def get_assistant_data(id):
+  assistant_data = AssistantData.objects(organization=id).first()
+  if not assistant_data:
+    raise DoesNotExist("Organization does not exist")
+  return jsonify(assistant_data), 200
