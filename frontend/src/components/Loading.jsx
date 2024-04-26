be3fd5c6c39cc455bef047d6be0eb6672628a102
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-04-26 16:55:21 +0700

    feat: prepare Loading component

---
+++ frontend/src/components/Loading.jsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return(
+    <main className="w-screen h-screen absolute top-0 left-0 bg-black flex justify-center items-center">
+      <h1>Loading</h1>
+    </main>
+  );
+}
