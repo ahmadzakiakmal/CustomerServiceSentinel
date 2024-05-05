425b076ba14f0920dfc5367b8028e855dee9cb65
azka <athillaazka2@gmail.com>
2024-05-05 22:11:35 +0700

    feat: adding testing text (#78)

--- frontend/src/pages/dashboard/index.jsx
+++ frontend/src/pages/dashboard/index.jsx
@@ -1,12 +1,15 @@
 import Layout from "@/components/Layout";
 
 export default function Dashboard() {
-  return(
+  return (
     <main>
       <Layout>
         <main className="text-dark-brown p-10">
           <h1 className="text-[24px] font-medium">Dashboard</h1>
         </main>
+        <div className="text-dark-brown p-10">
+          <h1>Hello World!</h1>
+        </div>
       </Layout>
     </main>
   );
