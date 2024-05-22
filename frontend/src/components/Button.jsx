ba430f51826287b520c0e5312f69ae4c0c39083b
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-05-22 08:01:35 +0700

    fix: button color

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -3,7 +3,7 @@ export default function Button({ onClick, children, className, disabled, type = 
     <button
       type={type}
       onClick={onClick}
-      className={`w-fit mt-4 rounded bg-dark-brown hover:bg-dark-brown/90 active:bg-dark-brown/80 transition px-4 py-2 text-white-bg text-lg text-center font-semibold disabled:opacity-60 disabled:cursor-not-allowed ${className}`}
+      className={`w-fit mt-4 rounded text-white bg-dark-brown hover:bg-dark-brown/90 active:bg-dark-brown/80 transition px-4 py-2 text-white-bg text-lg text-center font-semibold disabled:opacity-60 disabled:cursor-not-allowed ${className}`}
       disabled={disabled}
     >
       {children}
