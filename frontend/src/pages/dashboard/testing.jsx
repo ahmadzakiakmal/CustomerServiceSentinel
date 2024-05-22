14a415ce3e41ce6fc48d4df88c47eaf43750d277
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-05-22 11:14:33 +0700

    feat: make testing page responsive

--- frontend/src/pages/dashboard/testing.jsx
+++ frontend/src/pages/dashboard/testing.jsx
@@ -279,8 +279,8 @@ export default function TestingDashboard() {
   return (
     <main>
       <Layout>
-        <main className="text-dark-brown flex min-h-screen overflow-y-hidden">
-          <section className="min-w-[340px] border-r border-[#CACACA] p-10 flex-shrink-0 relative max-h-screen overflow-y-auto">
+        <main className="text-dark-brown flex flex-col md:flex-row min-h-screen overflow-y-hidden">
+          <section className="min-w-[340px] border-r border-[#CACACA] p-10 flex-shrink-0 relative max-h-screen overflow-y-auto md:outline-none border-b">
             {isLoading && (
               <div className="w-full h-full bg-dark-brown/60 backdrop-blur-[8px] absolute left-0 top-0 z-[2] flex justify-center items-center">
                 <h1 className="text-[25px] font-semibold animate-pulse text-white">Loading...</h1>
@@ -518,7 +518,7 @@ export default function TestingDashboard() {
             </Button>
           </section>
 
-          <section className="w-full flex flex-col max-h-screen relative">
+          <section className="w-full flex flex-col min-h-screen max-h-screen relative">
             <div className="flex w-full justify-between items-center p-10">
               <h1 className="text-[24px] font-medium">Chat</h1>
               <button
@@ -570,7 +570,7 @@ export default function TestingDashboard() {
                 )}
               </div>
               <form
-                className="pb-2 pt-4 px-10 absolute bottom-0 w-full bg-white border-t gap-2"
+                className="pb-2 pt-4 px-5 md:px-10 absolute bottom-[90px] md:bottom-0 w-full bg-white border-t gap-2"
                 onSubmit={(e) => {
                   sendMessage(e);
                 }}
