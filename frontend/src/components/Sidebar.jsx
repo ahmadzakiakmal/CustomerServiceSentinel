797762eae6d27c4e82ea5ee442f2381310ca78b4
Ahmad Zaki Akmal <87590846+ahmadzakiakmal@users.noreply.github.com>
2024-05-26 14:45:34 +0700

    fix: missing closing tag

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -17,34 +17,35 @@ const Sidebar = () => {
         <div className="profile-section text-white flex flex-col justify-center items-center mb-[75px]">
           <div className="profile-picture bg-white size-[60px] rounded-full mb-5" />
           <h2 className="text-[18px] hidden md:block font-semibold">Username</h2>
-        <div className="links-section flex flex-col gap-10 px-[30px] lg:px-[50px]">
-          <SidebarLink
-            text="Testing"
-            link="/dashboard/testing"
-            icon={<MdNoteAlt />}
-            active={router.pathname === "/dashboard/testing"}
-          />
-          <SidebarLink
-            text="Appearance"
-            link="/dashboard/appearance"
-            icon={<IoChatboxEllipses />}
-            active={router.pathname === "/dashboard/appearance"}
-          />
-          <SidebarLink
-            text="Organization"
-            link="/dashboard/organization"
-            icon={<GrOrganization />}
-            active={router.pathname === "/dashboard/organization"}
-          />
+          <div className="links-section flex flex-col gap-10 px-[30px] lg:px-[50px]">
+            <SidebarLink
+              text="Testing"
+              link="/dashboard/testing"
+              icon={<MdNoteAlt />}
+              active={router.pathname === "/dashboard/testing"}
+            />
+            <SidebarLink
+              text="Appearance"
+              link="/dashboard/appearance"
+              icon={<IoChatboxEllipses />}
+              active={router.pathname === "/dashboard/appearance"}
+            />
+            <SidebarLink
+              text="Organization"
+              link="/dashboard/organization"
+              icon={<GrOrganization />}
+              active={router.pathname === "/dashboard/organization"}
+            />
+          </div>
         </div>
-      </div>
 
-      <div className="logout-section w-full px-[50px] mt-[200px]">
-        <Link href="/">
-          <button className="logout-button block text-[18px] font-medium rounded-[5px] px-3 py-2 hover:bg-light-yellow/20 active:bg-light-yellow/50 transition w-full">
+        <div className="logout-section w-full px-[50px] mt-[200px]">
+          <Link href="/">
+            <button className="logout-button block text-[18px] font-medium rounded-[5px] px-3 py-2 hover:bg-light-yellow/20 active:bg-light-yellow/50 transition w-full">
             Log Out
-          </button>
-        </Link>
+            </button>
+          </Link>
+        </div>
       </div>
     </aside>
   );
